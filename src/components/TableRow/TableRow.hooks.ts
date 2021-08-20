1d9d8f47e9bfe63594e02967150f4bab2017a314
albert <trott.alik@gmail.com>
2021-08-20 12:37:17 +0300

    refactor: update useTableRow

--- src/components/TableRow/TableRow.hooks.ts
+++ src/components/TableRow/TableRow.hooks.ts
@@ -1,5 +1,5 @@
 import {useCallback} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import {shallowEqual, useDispatch, useSelector} from 'react-redux';
 
 import {toggleRowSelection} from 'src/redux/modules/users/actions';
 import {IState} from 'src/redux/store';
@@ -7,10 +7,11 @@ import {IState} from 'src/redux/store';
 export const useTableRow = ({id}: { id: string }) => {
   const dispatch = useDispatch();
 
-  const {firstName, age, lastName} = useSelector((state: IState) => state.users.byId[id]);
+  const {firstName, age, lastName} = useSelector((state: IState) => state.users.byId[id],
+    shallowEqual);
   const isSelected = useSelector((state: IState) => state.users.selectedIds.includes(id));
 
-  const handleSelect = useCallback(() => dispatch(toggleRowSelection(id)), [dispatch]);
+  const handleSelect = useCallback(() => dispatch(toggleRowSelection(id)), [dispatch, id]);
 
   return {
     age,
